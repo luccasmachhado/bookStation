b301b34c9e9870a396ce65b1e0a7e3d5b9a300e8
Lucas Machado <lucasmachado.2018@outlook.com>
2025-12-11 21:08:02 -0300

    bookstation

---
+++ controladores/livro.js
@@ -0,0 +1,88 @@
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } = require("../servicos/livro")
+
+function getLivros(req, res) {
+    try{
+        const livros = getTodosLivros()
+        res.send(livros)
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function getLivro(req, res) {
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)){
+            const livros = getLivroPorId(id)
+            res.send(livros)
+        }else{
+            res.status(422)
+            res.send("ID inválido")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res){
+    try{
+        if(req.body.nome){
+            const livroNovo = req.body
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso!")
+        }else{
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res){
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)){
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso!")
+        }else{
+            res.status(422)
+            res.send("ID inválido")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res) {
+    try{
+        const id = req.params.id
+        
+        if(id && Number(id)){
+            deletaLivroPorId(id)
+            res.send("Livro deletado com sucesso")
+        }else{
+            res.status(422)
+            res.send("ID inválido")
+        }
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+module.exports = {
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
+    deleteLivro
+}
